be14f62d38d087740374297178412f8da08993b3
C4piti44 <97689075+C4piti44@users.noreply.github.com>
2022-07-10 03:09:03 +0300

    Update the_server.py

--- the_server.py
+++ the_server.py
@@ -3,54 +3,91 @@ import threading
 import datetime
 
 
+
 server = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-server.bind(("0.0.0.0" ,  44444))
+server.bind(("0.0.0.0" ,  56000))
 server.listen()
 print("server is up and running")
+file = open("C:\\Users\\amitg\\Desktop\\secret.txt" , 'a')
+file.write(str(datetime.date.today()))
+file.write("\n")
+file.close()
 
-clients = []
-nicknames = []
-
-
+cli_nick = {} #contians all of the clients and their nicknames
 
 def broadcast(message):
-    for client in clients:
+    for client in cli_nick:
         client.send(message)
+
+def get_client2(dic , nickname):
+    for client in dic:
+        if (nickname == dic[client]):
+            client2 = client
+    return client2
+
+
+#def switch(client):
+    client.send("Which user would you like to talk with?".encode())
+    nickname = client.recv(1024).decode()
+    client2 = get_client2(cli_nick , nickname)
+    while True:
+        try:
+            message = client.recv(1024) #maybe the message should be encoded
+            if(message.decode().split(" " , 2)[2]=="EXIT"):
+                break
+            client2.send(message)
+        except:
+            client.send(f"An error occurred, you can't message {nickname} please try later.".encode())
+            print("An error occurred1")
+            break
+
 
 def handle(client):
     while True:
         try:
             message = client.recv(1024)
-            if((message.decode()).split(" " , 1)[1] == "WHORU"):
+            if((message.decode()).split(" " , 2)[2] == "WHORU"):
                 client.send("I am yor father!".encode())
-            else:
-                broadcast(message)
+                pass
+            if((message.decode()).split(" " , 2)[2] == "EXIT"):
+                raise Exception
+            #elif((message.decode()).split(" " , 2)[2]=="CNG"):
+                #switch(client)
+                #pass
+            
+            broadcast(message)
  
         except:
-            index = clients.index(client)
-            clients.remove(client)
-            print("An error occurred")
+            nickname = cli_nick[client]
+            cli_nick.pop(client)
             client.close()
-            nickname = nicknames[index]
             broadcast(f'{nickname} disconnected!'.encode())
-            nicknames.remove(nickname)
+            print(f'{nickname} disconnected!')
+            file = open("C:\\Users\\amitg\\Desktop\\secret.txt" , 'a')
+            file.write(f'{nickname} disconnected!\n')
+            file.close()
             break
 
-def receive():
+def receive_clients(): #this function receive new connections
     while True:
         client , address = server.accept()
         print(f'{str(address)} connected')
         
+
         client.send("please choose a nickname ".encode())
         nickname = client.recv(1024).decode()        
-        clients.append(client)
-        nicknames.append(nickname)
+        cli_nick[client] = nickname
         print(f"the nickname of {address} is {nickname} ")
+        file = open("C:\\Users\\amitg\\Desktop\\secret.txt" , 'a')
+        file.write(f"the nickname of {address} is {nickname} ")
+        file.write("\n")
+        file.close()
+
         broadcast(f'{nickname} has joined the chat'.encode())
         client.send("Welcome, you officially joined my chat hope you will enjoy".encode())
 
-        thread = threading.Thread(target=handle ,args=(client,))
+
+        thread = threading.Thread(target=handle ,args=(client, ))
         thread.start()
 
-
-receive()
+receive_clients()
